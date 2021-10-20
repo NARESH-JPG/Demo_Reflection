89349fee3d9537992a890dfe3d417105eee68a0d
NARESH <64158150+NARESH-JPG@users.noreply.github.com>
2021-10-20 14:00:34 +0530

    Day_6

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Reflection;
+
+
+namespace Demo_Reflection
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Demo_Reflection in C#.....!!!");
+            Type T = typeof(int);
+            Console.WriteLine("Type of name :{0}", T.Name);
+            Console.WriteLine("Full name of th type defined : \t{0}", T.FullName);
+            Console.WriteLine("Type of Assembly in which it is defined: \t{0}", T.Assembly);
+            Console.WriteLine("information about base type  : \t{0}", T.BaseType);
+
+            //creating an object of class reflection_class
+            Dynamic_Object Myobj1 = new Dynamic_Object();
+            Myobj1.First_Method();
+            Myobj1.Second_Method();
+
+
+            dynamic Myobj2 = new Dynamic_Object();
+            Myobj2.First_Method();
+            Myobj2.Second_Method();
+        
+        }
+
+    }
+}
---
+++ Reflection_Class.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_Reflection
+{
+    class Dynamic_Object
+    {
+        public Dynamic_Object() { }
+
+        public Dynamic_Object(string Msg) { }
+
+        public void First_Method() {
+            Console.WriteLine("Inside first method");
+        }
+
+        public void Second_Method() {
+            Console.WriteLine("Inside Seccond method");
+        }
+
+    }
+}
